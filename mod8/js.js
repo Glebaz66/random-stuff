9476d1741cfa5080ddc98dfd8adc7a41cc9e536d
gleb <glebaz666@gmail.com>
2019-06-07 13:48:25 +0300

    del img, replace with randim links

--- mod8/js.js
+++ mod8/js.js
@@ -1,10 +1,10 @@
 const galleryItems = [
-    { preview: 'img/preview-1.jpg', fullview: 'img/fullview-1.jpg', alt: "alt text 1" },
-    { preview: 'img/preview-2.jpg', fullview: 'img/fullview-2.jpg', alt: "alt text 2" },
-    { preview: 'img/preview-3.jpg', fullview: 'img/fullview-3.jpg', alt: "alt text 3" },
-    { preview: 'img/preview-4.jpg', fullview: 'img/fullview-4.jpg', alt: "alt text 4" },
-    { preview: 'img/preview-5.jpg', fullview: 'img/fullview-5.jpg', alt: "alt text 5" },
-    { preview: 'img/preview-6.jpg', fullview: 'img/fullview-6.jpg', alt: "alt text 6" },
+    { preview: 'https://picsum.photos/1280/960?random=1', fullview: 'https://picsum.photos/1280/960?random=1', alt: "alt text 1" },
+    { preview: 'https://picsum.photos/1280/960?random=2', fullview: 'https://picsum.photos/1280/960?random=2', alt: "alt text 2" },
+    { preview: 'https://picsum.photos/1280/960?random=3', fullview: 'https://picsum.photos/1280/960?random=3', alt: "alt text 3" },
+    { preview: 'https://picsum.photos/1280/960?random=4', fullview: 'https://picsum.photos/1280/960?random=4', alt: "alt text 4" },
+    { preview: 'https://picsum.photos/1280/960?random=5', fullview: 'https://picsum.photos/1280/960?random=5', alt: "alt text 5" },
+    { preview: 'https://picsum.photos/1280/960?random=6', fullview: 'https://picsum.photos/1280/960?random=6', alt: "alt text 6" },
   ];
 
   
